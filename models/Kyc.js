23a840c81f25b0e1c50423669f265093e67fd63f
nirajniit <niit.delhi.ind@gmail.com>
2025-02-11 18:39:34 +0530

    second commit

--- models/Kyc.js
+++ models/Kyc.js
@@ -1,110 +1,3 @@
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-
-// const kycSchema = new Schema({
-//     image: {
-//         type: String, 
-//         required: true
-//     },
-//     imageBack: {
-//         type: String,
-//         default: ""
-//     },
-//     type: {
-//         type: String,
-//         required: true
-//     },
-//     isVerify: {
-//         type: Number,
-//         required: true
-//     },
-//     uid: {
-//         type: Schema.Types.ObjectId,
-//         required: true,
-//         ref: 'User' 
-//     },
-//     cd: {
-//         type: Date,
-//         required: true
-//     },
-//     reason: {
-//         type: String, 
-//         default: ""
-//     },
-//     isRejected: {
-//         type: Number, 
-//         required: true
-//     },
-//     details: {
-//         age: {
-//             type: Number, 
-//             required: true
-//         },
-//         date_of_birth: {
-//             type: Date,
-//             required: true
-//         },
-//         date_of_issue: {
-//             type: Date, 
-//             default: ""
-//         },
-//         fathers_name: {
-//             type: String, 
-//             required: true
-//         },
-//         id_number: {
-//             type: String, 
-//             required: true
-//         },
-//         is_scanned: {
-//             type: Boolean, 
-//             required: true
-//         },
-//         minor: {
-//             type: Boolean, 
-//             required: true
-//         },
-//         name_on_card: {
-//             type: String,
-//             required: true
-//         },
-//         pan_type: {
-//             type: String, 
-//             required: true
-//         }
-//     },
-//     id_number: {
-//         type: String,
-//         required: true
-//     },
-//     apiResponce: {
-//         type: String, 
-//         required: true
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     updatedAt: {
-//         type: Date, 
-//         default: Date.now
-//     },
-//     cd_ist: {
-//         type: Date, 
-//         required: true
-//     }
-// },
-// { timestamps: true,
-//   collection:'kycData'
-
-//  }
-// );
-
-// const KYC = mongoose.model('KYC', kycSchema);
-
-// module.exports = KYC;
-
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const kycSchema = new Schema(
@@ -116,12 +9,16 @@ const kycSchema = new Schema(
     imageBack: {
       type: String, 
     },
+    id:{
+      type:String,
+      required:true
+    },
     type: {
       type: String,
       enum: ['pan', 'aadhar'], // Either PAN or Aadhar card
       required: true,
     },
-    isVerify: {
+    isVerified: {
       type: Boolean, 
       required: true,
     },
